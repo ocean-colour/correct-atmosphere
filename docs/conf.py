d5843fdf038b30adf8c3ab84e97756959dbaaa60
profxj <xavier@ucolick.org>
2026-01-01 15:59:02 -0800

    readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -5,15 +5,25 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
+
+# Add the package to the path (correct_atmosphere is in the root, not src/)
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'correct_atmosphere'
-copyright = '2024, correct_atmosphere contributors'
+copyright = '2025, correct_atmosphere contributors'
 author = 'correct_atmosphere contributors'
-release = '0.1.0'
+
+# The version info for the project you're documenting
+try:
+    from correct_atmosphere import __version__
+    release = __version__
+except ImportError:
+    release = '0.1.0'
+
+version = '.'.join(release.split('.')[:2])
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
